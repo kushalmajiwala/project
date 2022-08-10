b5bf164e4d33b4fa3d7c3e17f603256bebf00062
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2022-08-10 19:11:14 +0530

    heightAdded

--- src/lib/login.svelte
+++ src/lib/login.svelte
@@ -50,7 +50,7 @@
         margin: 0;
         padding: 0;
         background-image: url("../assets/images/signup-bg1.jpg");
-        /* height: 100vh; */
+        height: 100vh;
         font-family: sans-serif;
         background-size: cover;
         background-repeat: no-repeat;
