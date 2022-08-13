33fb0c1ee2da99bafb604bc321a3d70251eded42
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2022-08-13 10:17:26 +0530

    userFoundDone

--- src/lib/home.svelte
+++ src/lib/home.svelte
@@ -1,7 +1,42 @@
 <script>
+    import { onMount } from "svelte";
+    import axios from "axios";
+    import Usernotfound from "./usernotfound.svelte";
+    let userFound = false;
+
+    function checkUserExist() {
+        const options = {
+            method: "GET",
+            url: "https://lsk35tbplh.execute-api.ap-south-1.amazonaws.com/Prod/api/login/username/" + username,
+        };
+        axios
+            .request(options)
+            .then(function (response) {
+                console.log(response.data);
+                userFound = true;
+            })
+            .catch(function (error) {
+                userFound = false;
+            });
+    }
+
+    onMount(async () => {
+        await checkUserExist();
+    });
+
     export let username;
 </script>
+
 <main>
-    {username}
-    <h1>Home Page</h1>
+    <div>
+        {#if userFound == true}
+            {username}
+            <h1>Home Page</h1>
+        {/if}
+    </div>
+    <div>
+        {#if userFound == false}
+            <Usernotfound />
+        {/if}
+    </div>
 </main>
