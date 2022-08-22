b2ac4bb848fb07419d84e1f357b0cfb3955c2527
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2022-08-22 15:06:36 +0530

    stillDecreased

--- src/Routes/dashboardhome.svelte
+++ src/Routes/dashboardhome.svelte
@@ -76,14 +76,14 @@
             width: 350px;
             line-height: 160px;
             margin-left: 70%;
-            margin-top: 52%;
+            margin-top: 50%;
         }
         .add-content2 {
             height: 160px;
             width: 350px;
             line-height: 160px;
             margin-left: 90%;
-            margin-top: 52%;
+            margin-top: 50%;
         }
     }
     @media screen and (max-width: 1250px) {
