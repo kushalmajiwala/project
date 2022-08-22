26e06629ab53eb2551b807585b926782d3487da9
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2022-08-22 16:27:36 +0530

    mycvAddAdded

--- src/Routes/mycv.svelte
+++ src/Routes/mycv.svelte
@@ -1,3 +1,139 @@
+<script>
+    import { Router, Link, Route } from "svelte-routing";
+
+    export let username;
+    export let url = "";
+</script>
+
 <main>
-    <h1>mycv</h1>
+    <Router {url}>
+        <div class="add-content">
+            <Link to="/{username}/manage/addcv" style="text-decoration: none;">
+            <i
+                class="bi bi-plus-circle-fill plusbtn"
+            />
+            </Link>
+            <p class="add-txt">New CV</p>
+        </div>
+    </Router>
 </main>
+
+<style>
+    .add-content {
+        margin-left: 65%;
+        margin-top: 20%;
+        position: fixed;
+    }
+    .add-txt {
+        text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        color: black;
+        margin-left: 0%;
+        margin-top: -25%;
+        font-size: 25px;
+    }
+    .plusbtn {
+        color:rgb(100, 86, 167); 
+        font-size:90px; 
+        cursor: pointer;
+    }
+    .plusbtn:hover {
+        color:rgb(85, 69, 155); 
+    }
+    /* @media screen and (min-width: 1350px) {
+        .add-content {
+            margin-left: 70%;
+            margin-top: 70%;
+        }
+    } */
+    @media screen and (max-width: 1230px) {
+        .add-content {
+            margin-left: 70%;
+            margin-top: 30%;
+        }
+    }
+    @media screen and (max-width: 1170px) {
+        .add-content {
+            margin-left: 70%;
+            margin-top: 33%;
+        }
+    }
+    @media screen and (max-width: 1100px) {
+        .add-content {
+            margin-left: 70%;
+            margin-top: 36%;
+        }
+    }
+    @media screen and (max-width: 1070px) {
+        .add-content {
+            margin-left: 67%;
+            margin-top: 38%;
+        }
+    }
+    @media screen and (max-width: 995px) {
+        .add-content {
+            margin-left: 85%;
+            margin-top: 25%;
+        }
+    }
+    @media screen and (max-width: 900px) {
+        .add-content {
+            margin-left: 85%;
+            margin-top: 27%;
+        }
+    }
+    @media screen and (max-width: 820px) {
+        .add-content {
+            margin-left: 85%;
+            margin-top: 30%;
+        }
+    }
+    @media screen and (max-width: 740px) {
+        .add-content {
+            margin-left: 85%;
+            margin-top: 35%;
+        }
+    }
+    @media screen and (max-width: 640px) {
+        .add-content {
+            margin-left: 83%;
+            margin-top: 40%;
+        }
+    }
+    @media screen and (max-width: 580px) {
+        .add-content {
+            margin-left: 80%;
+            margin-top: 45%;
+        }
+    }
+    @media screen and (max-width: 500px) {
+        .add-content {
+            margin-left: 78%;
+            margin-top: 50%;
+        }
+    }
+    @media screen and (max-width: 450px) {
+        .add-content {
+            margin-left: 75%;
+            margin-top: 55%;
+        }
+    }
+    @media screen and (max-width: 400px) {
+        .add-content {
+            margin-left: 70%;
+            margin-top: 65%;
+        }
+    }
+    @media screen and (max-width: 360px) {
+        .add-content {
+            margin-left: 70%;
+            margin-top: 70%;
+        }
+    }
+    @media screen and (max-width: 340px) {
+        .add-content {
+            margin-left: 67%;
+            margin-top: 75%;
+        }
+    }
+</style>
+
