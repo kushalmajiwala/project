3a57da1bfdb93bb9c437aea3b0170a6ea91100ae
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2022-08-22 15:26:03 +0530

    navbarRemoved

--- src/Routes/addcv.svelte
+++ src/Routes/addcv.svelte
@@ -123,11 +123,9 @@
         margin-top: 2%;
     }
     @media screen and (min-width: 1350px) {
-      .navbar {
-        padding-left: 33%;
-      }
+      
       .my-content {
-        width: 75.5%;
+        width: 76%;
       }
     }
     @media screen and (max-width: 1230px) {
