f26edc77538064191d7e8f14b20194e25b7b3837
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2022-12-12 17:55:30 +0530

    adminSideReady

--- src/lib/admindashboard.svelte
+++ src/lib/admindashboard.svelte
@@ -10,18 +10,31 @@
         Modal,
         ModalFooter,
         ModalHeader,
+        Tooltip,
     } from "sveltestrap";
     import { onMount } from "svelte";
     import Plotly from "plotly.js-dist-min";
+
+    let no_feedback_style = "color:aliceblue;"
 
     let total_user = 0;
     let total_cv = 0;
     let total_letter = 0;
 
+    let totalFeedback = [];
+    let checking = false;
+
     let dark = true;
     let light = false;
+    let delete_fid = "";
 
-    onMount(async () => {
+    let open1 = false;
+    let open2 = false;
+
+    const toggle1 = () => (open1 = !open1);
+    const toggle2 = () => (open2 = !open2);
+
+    function getData() {
         //Total User
         const options1 = {
             method: "GET",
@@ -110,10 +123,48 @@
             .catch(function (error) {
                 console.error(error);
             });
+    }
+    function checkFeedbackExist() {
+        //Fetching Feedback Data
+        const options = {
+            method: "GET",
+            url: "https://lsk35tbplh.execute-api.ap-south-1.amazonaws.com/Prod/api/feedback/",
+        };
+
+        axios
+            .request(options)
+            .then(function (response) {
+                totalFeedback = [];
+                totalFeedback.push(response.data);
+                console.log(totalFeedback[0].length);
+                if (totalFeedback[0].length == 0) {
+                    checking = false;
+                    if(dark)
+                    {
+                        no_feedback_style = "color:aliceblue;";
+                    }
+                    else
+                    {
+                        no_feedback_style = "color:black;";
+                    }
+                } else {
+                    checking = true;
+                }
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    }
+    onMount(async () => {
+        await getData();
+        await checkFeedbackExist();
     });
     function togglebtn() {
         if (dark) {
             document.getElementById("main").style.backgroundColor = "aliceblue";
+            if (checking == false) {
+                no_feedback_style = "color:black;";
+            }
             dark = false;
         } else {
             dark = true;
@@ -121,18 +172,45 @@
         if (light) {
             document.getElementById("main").style.backgroundColor =
                 "rgb(10, 9, 10)";
+            if (checking == false) {
+                no_feedback_style = "color:aliceblue;";
+            }
             light = false;
         } else {
             light = true;
         }
+    }
+    function setDeleteFeedback(fid) {
+        delete_fid = fid;
+        toggle1();
+    }
+    function deleteFeedback(fid) {
+        const options = {
+            method: "DELETE",
+            url:
+                "https://lsk35tbplh.execute-api.ap-south-1.amazonaws.com/Prod/api/feedback/fid/" +
+                fid,
+        };
+
+        axios
+            .request(options)
+            .then(function (response) {
+                toggle2();
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
     }
 
     export let username;
 </script>
 
 <main class="main" id="main">
-    <nav class="navbar navbar-dark bg-dark" style="color: white;">
-        <div class="togglebtn" on:click={togglebtn}>
+    <nav class="navbar navbar-dark bg-dark navbar-content" style="color: white;">
+        <div class="admin">
+            <i class="bi bi-person-square"></i>&nbsp Admin
+        </div>
+        <div class="togglebutton" on:click={togglebtn}>
             {#if dark}
                 <p>
                     <i class="bi bi-brightness-high" /><span
@@ -195,14 +273,162 @@
         </div>
     </div>
     <div class="charts">
-        <div id="barPlot" class="bar-plot"/>
-        <div id="piePlot" class="pie-plot"/>
+        <div id="barPlot" class="bar-plot" />
+        <div id="piePlot" class="pie-plot" />
+    </div>
+    <div class="feedback-container">
+        <div class="navbar navbar-dark bg-dark feedback-header">
+            <p>All-Feedback</p>
+        </div>
+        <div class="all-feedbacks">
+            {#if checking == false}
+                <div class="no-feedback" id="no-feedback" style={no_feedback_style}>
+                    <p>No Feedback</p>
+                </div>
+            {:else}
+                {#each totalFeedback[0] as rec, i}
+                    <div class="Tooltip">
+                        <Tooltip target="delete-{i}" placement="bottom">
+                            Delete
+                        </Tooltip>
+                    </div>
+                    <div class="feedback-inner">
+                        <div class="card col-sm-6 myinnercard">
+                            <table>
+                                <h5 class="card-header">
+                                    <th style="width: 100%;">{rec.username}</th
+                                    ><th
+                                        ><i
+                                            class="bi bi-trash-fill deletebtn"
+                                            id="delete-{i}"
+                                            on:click={() =>
+                                                setDeleteFeedback(rec.fid)}
+                                        /></th
+                                    >
+                                </h5>
+                            </table>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    {rec.subject}
+                                </h5>
+                                <p class="card-text">
+                                    {rec.message}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                {/each}
+            {/if}
+        </div>
+    </div>
+    <div class="Modals">
+        <!-- Confirm Delete -->
+        <Modal isOpen={open1}>
+            <ModalFooter>
+                <div class="delete-symbol-container">
+                    <p style="font-size: 30px; font-weight: 500;">
+                        DELETE FEEDBACK
+                    </p>
+                </div>
+                <div class="delete-symbol-container">
+                    <i class="bi bi-exclamation-circle delete-symbol" />
+                </div>
+                <br />
+                <div class="delete-symbol-container">
+                    <p class="delete-txt">
+                        Are you sure you want to delete this Feedback ?
+                    </p>
+                </div>
+                <Button color="primary" on:click={toggle1}>Cancle</Button>
+                <Button
+                    color="danger"
+                    on:click={toggle1}
+                    on:click={() => deleteFeedback(delete_fid)}>Delete</Button
+                >
+            </ModalFooter>
+        </Modal>
+        <!-- Deleted Successfully... -->
+        <Modal isOpen={open2}>
+            <ModalFooter>
+                <div class="delete-symbol-container">
+                    <i class="bi bi-check-circle deleted-symbol" />
+                </div>
+                <div class="delete-symbol-container">
+                    <p class="delete-txt">Feedback is Deleted</p>
+                </div>
+                <Button
+                    color="primary"
+                    on:click={toggle2}
+                    on:click={checkFeedbackExist}
+                    style="width: 100px;">OK</Button
+                >
+            </ModalFooter>
+        </Modal>
     </div>
 </main>
 
 <style>
-    .bar-plot, .pie-plot {
+    .admin {
+        font-size: 30px;
+    }
+    .navbar-content {
+        display: flex;
+        justify-content: space-between;
+    }
+    .delete-txt {
+        font-size: 20px;
+    }
+    .delete-symbol-container {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+    }
+    .delete-symbol {
+        font-size: 70px;
+        color: rgb(234, 187, 100);
+    }
+    .deleted-symbol {
+        font-size: 70px;
+        color: rgb(39, 138, 63);
+    }
+    .deletebtn {
+        font-size: 30px;
+        color: red;
+        cursor: pointer;
+    }
+    .no-feedback {
+        font-size: 30px;
+        display: flex;
+        justify-content: center;
+    }
+    .myinnercard {
+        box-shadow: 0px 0px 7px 0px rgb(108, 105, 105);
+    }
+    .feedback-inner {
+        display: flex;
+        justify-content: center;
+        margin-top: 3%;
+    }
+    .all-feedbacks {
+        margin-top: 3%;
+        padding-bottom: 3%;
+    }
+    .feedback-container {
+        margin-top: 5%;
+    }
+    .feedback-header {
+        display: flex;
+        justify-content: center;
+        color: white;
+        font-size: 30px;
+        height: 80px;
+        line-height: 60px;
+    }
+    .bar-plot,
+    .pie-plot {
         width: 500px;
+        box-shadow: 0px 0px 7px 0px rgb(108, 105, 105);
     }
     .charts {
         display: flex;
@@ -219,7 +445,17 @@
         cursor: pointer;
         margin-left: 90%;
     }
-    .togglebtn:hover {
+    .togglebutton {
+        background-color: rgb(31, 30, 31);
+        color: white;
+        height: 50px;
+        width: 150px;
+        border-radius: 15px;
+        text-align: center;
+        line-height: 50px;
+        cursor: pointer;
+    }
+    .togglebutton:hover {
         background-color: rgb(45, 45, 45);
     }
     .inner-user-total {
@@ -282,6 +518,7 @@
         border-radius: 25px;
         color: white;
         font-size: 25px;
+        box-shadow: 0px 0px 7px 0px rgb(108, 105, 105);
     }
     @media screen and (max-width: 1100px) {
         .main-container {
@@ -360,7 +597,8 @@
             display: block;
             padding-left: 8%;
         }
-        .pie-plot, .bar-plot {
+        .pie-plot,
+        .bar-plot {
             width: 400px;
             height: 400px;
         }
@@ -374,7 +612,8 @@
             display: block;
             padding-left: 10%;
         }
-        .pie-plot, .bar-plot {
+        .pie-plot,
+        .bar-plot {
             width: 350px;
             height: 350px;
         }
